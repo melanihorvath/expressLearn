3956ee725583abdda0b026071586b39f8ac86e98
melanihorvath <144622336+melanihorvath@users.noreply.github.com>
2023-12-11 14:07:38 +0100

    introdution tasks

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Ciao');
+  });
+  
+  app.listen(3000, () => {
+    console.log(`Open this link in your browser: http://127.0.0.1:3000`);
+  });
+
+  app.get('/another-path', (req, res) => {
+    res.send('Ciao on another path');
+  });
+
+  app.get('/math/:op', (req, res) => {
+
+    const x = parseFloat(req.query.x);
+    const y = parseFloat(req.query.y);
+  
+    const {op} = req.params;
+  
+    const result = op === "add" ? x + y : op === "subtract" ? x - y : op === "multiply" ? x * y : op === "divide" ? x / y : false;
+  
+    const resultObject = {
+      numbers: {
+        x: x,
+        y: y
+      },
+      operation: op,
+      result: result ? result : `Unrecognizable operation name`
+    }
+  
+    res.send(resultObject);
+  
+  });
